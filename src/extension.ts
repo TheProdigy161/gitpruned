d437a7664de429facf9273c32a22fa2b0c748db5
Ross Bratton <RossB0918@randox.com>
2021-06-02 17:02:25 +0100

    Updated to change name

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,57 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import { exec, execSync } from 'child_process';
-import { commands, ExtensionContext } from 'vscode';
+import { commands, DocumentHighlight, ExtensionContext, window, workspace } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	// let output = createOutputChannel("Cool Trimmings");
-	console.log('Congratulations, your extension "cooltrimmings" is now active!');
+	const output = window.createOutputChannel("Git Prun'd");
+	output.show(true);
+	output.appendLine('Congratulations, your extension "gitpruned" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = commands.registerCommand('cooltrimmings.getBranchesList', () => {
-		// cmd.exe git branch -a
-		const currentDirectory = __dirname;
+	let disposable = commands.registerCommand('gitpruned.clearBranches', () => {
+		output.appendLine("Pruning branches.");
+		const currentWorkspaceFolders: string[] | undefined = workspace.workspaceFolders?.map(folder => folder.uri.path);
+		let currentDirectory: string = "";
 
-		exec(`cd ${__dirname} && git branch -a`, (e, x, y) => {
+		if (currentWorkspaceFolders != null)
+			currentDirectory = currentWorkspaceFolders[0].substring(1);
+		else {
+			output.appendLine("Failed to get a workspace folder.");
+			return;
+		}
+		
+		exec(`cd ${currentDirectory} && git branch -a`, (e, x, y) => {
 			if (e) {
-				console.error(`Exec error: ${e}`);
+				output.appendLine(`Exec error: ${e}`);
 				return;
 			}
-				
-			console.log(x);
 
-			// execSync(`cmd.exe git branch --delete --force ${branchName}`)
+			const branches: string[] = x.split('\n').filter(x => !x.includes('*'));
+
+			branches.forEach(x => {
+				x = x.trim();
+
+				if (x == null)
+					return;
+
+				output.appendLine(`Pruning branch: ${x}`);
+				try {
+					execSync(`cd ${currentDirectory} && git branch --delete --force ${x}`);
+					output.appendLine('Branch pruned.');
+				}
+				catch (e) {
+					output.appendLine(`Error: ${e}`);
+				}
+			});
+			
+			output.appendLine("Get prun'd kid.");
+			output.appendLine(`Pruned ${branches.length} branches.`);
 		});
-		let branchNames: string[] = [];
-		
-		branchNames.forEach(branchName => {
-			execSync(`cmd.exe git branch --delete --force ${branchName}`)
-		});
-		
-		// Display a message box to the user
-		// window.showInformationMessage('Hello World from CoolTrimmings!');
 	});
 
 	context.subscriptions.push(disposable);
