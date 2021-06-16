34db557e47eb7a54d8b80cd31b77d2d57738b309
Ross Bratton <RossB0918@randox.com>
2021-06-16 09:15:12 +0100

    Updated to change delete dropdown to delete button

--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,21 @@ import { Utilities } from './utilities';
 import { BranchesProvider } from './branchesProvider';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { exec, execSync } from 'child_process';
-import { commands, ExtensionContext, window, workspace } from 'vscode';
+import { commands, ExtensionContext, TreeViewOptions, window, workspace } from 'vscode';
 import { Branch } from './branch';
-
-let currentDirectory: string = Utilities.getCurrentDirectory();
 
 // This method is called when the extension is activated.
 export function activate(context: ExtensionContext) {
 	const branchesProvider: BranchesProvider = new BranchesProvider();
-	Utilities.showOutputChannel();
+	let treeViewOptions: TreeViewOptions<Branch> = {
+		treeDataProvider: branchesProvider,
+		canSelectMany: true
+	};
+	
+	// Utilities.showOutputChannel();
 	Utilities.log('Congratulations, your extension "gitpruned" is now active!');
 	
-	window.registerTreeDataProvider('gitPrunedBranches', branchesProvider);
+	window.createTreeView('gitPrunedBranches', treeViewOptions);
 	commands.registerCommand("gitpruned.refreshBranches", () => branchesProvider.refresh());
 	commands.registerCommand("gitpruned.deleteBranch", (branch: Branch) => branchesProvider.delete(branch));
 }
